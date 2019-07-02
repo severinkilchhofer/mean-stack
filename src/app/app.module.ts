82d151d12d62a9ba3e826e5d0a99645281f2501f
Severin Kilchhofer <>
2019-07-02 22:35:27 +0200

    added routing added edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,24 +1,48 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
-
-import { PostsService } from "../posts.service";
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {PostsService} from '../posts.service';
+import {Post} from '../post.model';
 
 @Component({
-  selector: "app-post-create",
-  templateUrl: "./post-create.component.html",
-  styleUrls: ["./post-create.component.css"]
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
-  enteredTitle = "";
-  enteredContent = "";
+export class PostCreateComponent implements OnInit {
+  enteredTitle = '';
+  enteredContent = '';
+  post: Post;
 
-  constructor(public postsService: PostsService) {}
+  private mode = 'create';
+  private postId = 'postId';
 
-  onAddPost(form: NgForm) {
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
+
+    if (this.mode === 'create') {
     this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
     form.resetForm();
   }
 }
